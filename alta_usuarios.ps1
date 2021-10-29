60ae0ee4305ffed8d30f3f9583e9a38c9b54a14a
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2021-10-29 17:19:52 +0200

    Update alta_usuarios.ps1

--- alta_usuarios.ps1
+++ alta_usuarios.ps1
@@ -1,1 +1,48 @@
 
+param($a,$b)
+$dominio=$a
+$sufijo=$b
+$dc="dc="+$dominio+",dc="+$sufijo
+
+
+if (!(Get-Module -Name ActiveDirectory)) #Accederá al then solo si no existe una entrada llamada ActiveDirectory
+{
+  Import-Module ActiveDirectory #Se carga el módulo
+}
+
+$fichero_csv=Read-Host "Introduce el fichero csv de los usuarios:"
+
+$fichero_csv_importado = import-csv -Path $fichero_csv -Delimiter : 			     
+foreach($linea_leida in $fichero_csv_importado)
+{
+	
+  	$rutaContenedor =$linea_leida.ContainerPath+","+$dc 
+
+	$name=$linea.Name
+	$nameShort=$linea.Name+'.'+$linea_leida.Surname
+	$Surnames=$linea.Surname+' '+$linea_leida.Surname2
+	$nameLarge=$linea.Name+' '+$linea_leida.Surname+' '+$linea_leida.Surname2
+	$computerAccount=$linea_leida.Computer
+	$email=$nameShort+"@"+$a+"."+$b
+
+	[boolean]$Habilitado=$true
+  	If($linea_leida.Hability -Match 'false') { $Habilitado=$false}
+  
+	New-ADUser `
+    		-SamAccountName $nameShort `
+    		-UserPrincipalName $nameShort `
+    		-Name $nameShort `
+		-Surname $Surnames `
+    		-DisplayName $nameLarge `
+    		-GivenName $name `
+    		-LogonWorkstations:$linea.Computer `
+		-Description "Cuenta de $nameLarge" `
+    		-EmailAddress $email `
+		-AccountPassword $passAccount `
+    		-Enabled $Habilitado `
+		-CannotChangePassword $false `
+    		-ChangePasswordAtLogon $true `
+		-PasswordNotRequired $false `
+    		-Path $rutaContenedor
+    		
+}
